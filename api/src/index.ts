bf640d2d00c8f3e34985485d3128c64b1bc77a97
Irosh Perera <iroshlashan31@gmail.com>
2024-08-08 16:00:42 +0530

    connect to database

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,16 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO as string).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+});
+
+
+
 
 const app = express();
 const port = 8000;
