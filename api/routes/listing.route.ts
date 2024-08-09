5d2475b58cb677a14a22da75b9668d99411eb291
Irosh Perera <iroshlashan31@gmail.com>
2024-08-09 15:38:38 +0530

    create update listing api routes

--- api/routes/listing.route.ts
+++ api/routes/listing.route.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createListing,deleteListing } from '../controller/listing.controller';
+import { createListing,deleteListing,updateListing } from '../controller/listing.controller';
 import { verifyUser } from '../utils/verifyUser';
 
 const router = express.Router();
 
 router.post('/create',verifyUser, createListing);
-router.delete('/delete/:id',verifyUser,deleteListing)
+router.delete('/delete/:id',verifyUser,deleteListing);
+router.post('/update/:id',verifyUser,updateListing);
 
 export default router;
