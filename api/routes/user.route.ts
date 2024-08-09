a22d150e3a7299870f88a8335157bd88265fe5fa
Irosh Perera <iroshlashan31@gmail.com>
2024-08-09 16:52:42 +0530

    add contact land load functionality to the listing page

--- api/routes/user.route.ts
+++ api/routes/user.route.ts
@@ -1,6 +1,6 @@
 
 import express from 'express';
-import { test, updateUser,deleteUser , getUserListing} from '../controller/user.controller';
+import { test, updateUser,deleteUser , getUserListing,getUser} from '../controller/user.controller';
 import { verifyUser } from '../utils/verifyUser';
 
 
@@ -10,6 +10,7 @@ router.get('/test', test);
 router.post('/update/:id',verifyUser, updateUser);
 router.delete('/delete/:id',verifyUser, deleteUser);
 router.get('/listing/:id',verifyUser, getUserListing);
+router.get('/:id', verifyUser, getUser)
 
 
 export default router;
